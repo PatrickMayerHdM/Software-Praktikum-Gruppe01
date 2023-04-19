011ac83d69b3e0fab28be7547b2d145aa7febbb9
Michal Fezer <mf186@hdm-stuttgart.de>
2023-04-19 15:57:17 +0200

    Account.py

--- backend/src/server/bo/Account.py
+++ backend/src/server/bo/Account.py
@@ -9,10 +9,6 @@ class Account(BusinessObject):
         self.google_id = google_id
         self.account_id = account_id
 
-    def set_accountID(self, account_id):
-        """Anlegen accountID"""
-        self.account_id = account_id
-
     def set_profile_id(self, profile_id):
         """Anlegen profile_id"""
         self.profile_id = profile_id
@@ -21,6 +17,13 @@ class Account(BusinessObject):
         """Auslesen google_id"""
         return self.google_id
 
+
+    def set_account_id(self, account_id):
+        """Anlegen accountID"""
+        self.account_id = account_id
+
+
     def get_account_id(self):
         """Auslesen account_id (TESTWEISE)"""
         return self.account_id
+
