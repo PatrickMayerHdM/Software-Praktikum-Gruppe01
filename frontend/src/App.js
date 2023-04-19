6f2d42ccdb9a4891ba026ed1135a73362251ea5f
ptrckmyr <pmayer2003@gmail.com>
2023-04-19 14:48:00 +0200

    npm install und save react-router-dom und hinzufügen von BlockNode.js und FavoriteNode.js und einer jeweils kleinen Beispielsfunktion. Dazu das Einbinden  in App.js damit der Inhalt unten kurz angezeigt wird.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import BlockNode from "./BlockNode";
+import FavoriteNode from "./FavoriteNode";
+import {Route, Link, Routes} from "react-router-dom";
 
 function App() {
   return (
@@ -18,6 +21,12 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Routes>
+          <Route path="/BlockNode" element={<BlockNode/>}/>
+          <Route path="/FavoriteNode" element={<FavoriteNode/>}/>
+      </Routes>
+
     </div>
   );
 }
---
+++ frontend/src/BlockNode.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+/* Funktion für die BlockNode / Kontaktsperre*/
+function BlockNode(){
+    return(
+        <div>
+            <h1>Kontaktsperre</h1>
+        </div>
+    )
+}
+
+export default BlockNode
