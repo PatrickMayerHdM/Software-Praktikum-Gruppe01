3610d8d851590aecb4c627a73d24d0addeb23010
Michal Fezer <mf186@hdm-stuttgart.de>
2023-04-20 16:16:27 +0200

    Klasse relationship ist hier eine abstractklasse welche als Vorlage für blockNote und favoriteNote dient.

--- backend/src/server/bo/relationship.py
+++ backend/src/server/bo/relationship.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class relationship(ABC):
-    """relationship ist die Superklasse von favoriteNote und BlockNote"""
 
     @abstractmethod
     def create_relation(self):
