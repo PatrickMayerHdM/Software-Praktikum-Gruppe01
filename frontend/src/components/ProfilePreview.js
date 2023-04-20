40db8930d602ad5d1ec07a781144a2d3988190da
ptrckmyr <pmayer2003@gmail.com>
2023-04-20 16:55:51 +0200

    Erstellen Grid für das ProfilePreview

--- frontend/src/components/ProfilePreview.js
+++ frontend/src/components/ProfilePreview.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Item from '../theme';
+
 
 /**
  * Erstellt einen react Komponenten für die Profilvorschau.
@@ -13,7 +17,22 @@ import React from "react";
 function ProfilePreview(){
     return(
         <div>
-            Test div for ProfilePreview
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <Item>xs=8</Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Item>xs=8</Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 }
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,8 +1,23 @@
-import { createTheme } from '@mui/material/styles';
+import {createTheme, styled} from '@mui/material/styles';
 import { colors } from '@mui/material';
+import Paper from "@mui/material/Paper";
+
 
 const white = '#FFFFFF';
 const black = '#000000';
+
+/*
+const für das Item Element des Grids in ProfilePreview
+ */
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default Item;
 
 //custom theme for this app
 /*in der Render Methode in App.js ist dann ((vgl. Bankprojekt Video 3))
