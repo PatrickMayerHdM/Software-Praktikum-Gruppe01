db508d8300ab2fb87b080c22f58e46472b027f50
Michal Fezer <mf186@hdm-stuttgart.de>
2023-04-24 14:54:21 +0200

    Klasse relationship ist hier eine abstractklasse welche als Vorlage für blockNote und favoriteNote dient.

---
+++ backend/src/server/bo/profile.py
@@ -0,0 +1,36 @@
+from BusinessObject import BusinessObject as bo
+
+
+class profile(bo):
+    def __init__(self):
+        super().__init__()
+        self.charsList = []
+        self.content = None
+        self.contact_id = None
+
+    def add_char(self, chars):
+        """Die Charasteristics werden der Liste self.charsList hinzugefügt"""
+        if chars not in self.charsList:
+            self.charsList.append(chars)
+
+    def del_chars(self, chars):
+        """Die Charasteristics werden von der Liste self.charsList entfernt"""
+        if chars in self.charsList:
+            self.charsList.remove(chars)
+
+    def show_char(self):
+        """Zeigt die Charesteristics an"""
+        return self.charsList
+
+
+
+
+
+
+
+"""
+A = profile()
+
+A.add_char("Raucher")
+print(A.show_char())
+"""
