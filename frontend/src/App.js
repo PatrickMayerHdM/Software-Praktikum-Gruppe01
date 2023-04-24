14095f9de9adcd68496c2ff84f5402c438f8611d
PatrickMayerHdM <pmayer2003@gmail.com>
2023-04-24 21:39:19 +0200

    Hinzufügen von Kommentaren zur Funktionalität der Profilebox.js und einer technischen validierung zur Prüfung ob eine Verschachtelung mehrerer Grids/ Boxen möglich ist.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-
+import Item from "./theme";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Profilebox from "./components/Profilebox";
 
 function App() {
   return (
     <div className="App">
-      App
+        {/** Beispiel, um die Möglichkeit zur Darstellung von mehreren Profileboxen zu gewährleisten. */}
+      <Box sx={{ flexGrow: 1 }} >
+          <Grid container spacing={2} >
+            <Grid item xs={7} >
+              <Item>
+                  <Profilebox/>
+              </Item >
+            </Grid >
+            <Grid item xs={7} >
+              <Item>
+                  <Profilebox/>
+              </Item >
+            </Grid >
+          </Grid >
+        </Box >
     </div>
   );
 }
