5d7016d6335d933a4868c844fbbd11c3c6333260
EfstratiosVassiliou <ev018@hdm-stuttgart.de>
2023-04-25 13:21:23 +0200

    Kommentare überarbeitet in App, LogIn und config Datei.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,11 @@ import Item from "./theme";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Profilebox from "./components/Profilebox";
-
 import OnBoarding from "./pages/OnBoarding";
 import LogIn from "./pages/LogIn";
 function App() {
   return (
-        // Darstellung der Test-Funktionen (LogIn-Page und OnBoarding-Page)
+        /** Darstellung der Test-Funktionen (LogIn-Page und OnBoarding-Page) */
     <div className="App">
      <LogIn/>
         <OnBoarding/>
@@ -32,6 +31,6 @@ function App() {
 
 export default App;
 
-/*Die Firebase Authentifizierung ist bei Kunz in App.js */
+/** Die Firebase Authentifizierung ist bei Kunz in App.js */
 
-/*Die Inhaltsstruktur findet sich auch in App.js (Siehe Bankprojekt)*/
+/** Die Inhaltsstruktur findet sich auch in App.js (Siehe Bankprojekt)*/
