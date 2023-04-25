43457a8fc48b47e8b02280861d953b3e9a695147
oresttkach <ot014@hdm-stuttgart.de>
2023-04-25 23:04:51 +0200

    Neue Navbar und Styling. Responsive Design ist bereits integriert.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,19 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Profilebox from "./components/Profilebox";
 
+import Navbar from "./components/header/Navbar";
+
 import OnBoarding from "./pages/OnBoarding";
 import LogIn from "./pages/LogIn";
+
+import "../src/Styles/header.css"
 
 function App() {
   return (
         // Darstellung der Test-Funktionen (LogIn-Page und OnBoarding-Page)
     <div className="App">
      <LogIn/>
+        <Navbar/>
         <OnBoarding/>
         {/** Beispiel, um die Möglichkeit zur Darstellung von mehreren Profileboxen zu gewährleisten. */}
       <Box sx={{ flexGrow: 1 }} >
---
+++ frontend/src/components/header/Navbar.js
@@ -0,0 +1,29 @@
+import React, {useRef} from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
+function Navbar() {
+    const navRef = useRef();
+
+    // Mit showNavbar wird die responsive Navbar realisiert
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+    return (
+        <header>
+            <h3>LOGO</h3>
+            <nav ref={navRef}>
+                <a href="/#">Home</a>
+                <a href="/#">Merkzettel</a>
+                <a href="/#">Sperrliste</a>
+                <a href="/#">Profil</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars/>
+            </button>
+        </header>
+    )
+}
+
+export default Navbar;
