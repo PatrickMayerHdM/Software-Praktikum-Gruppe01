83491c79e19b9e503bde64a61a04fa649b076fc4
PatrickMayerHdM <pmayer2003@gmail.com>
2023-04-26 12:21:31 +0200

    Erstellen einer expliziten Test Datei "TestProfileList" zum darstellen von mehreren Profileboxen in einem Grid.

---
+++ frontend/src/components/TestProfileList.js
@@ -0,0 +1,33 @@
+import Item from "../theme";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Profilebox from "./Profilebox";
+
+/** Dies ist eine Test Seite, zum darstellen mehrerer Profileboxen innerhalb eines weiteren Grids.
+ * Damit dies dann nicht mehr innerhalb der App.js geschehen muss und die App.js dadurch übersichtlicher bleibt.*/
+
+function TestProfileList(){
+    return(
+        <Box sx={{ flexGrow: 1 }} >
+          <Grid container spacing={2} >
+            <Grid item xs={7} >
+              <Item>
+                  <Profilebox/>
+              </Item >
+            </Grid >
+            <Grid item xs={7} >
+              <Item>
+                  <Profilebox/>
+              </Item >
+            </Grid >
+            <Grid item xs={7} >
+              <Item>
+                  <Profilebox/>
+              </Item >
+            </Grid>
+          </Grid >
+        </Box >
+        )
+
+}
+export default TestProfileList
