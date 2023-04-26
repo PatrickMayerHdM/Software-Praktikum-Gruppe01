9371f90e599f63570645ef403ce3ed1cc939d3d3
oresttkach <ot014@hdm-stuttgart.de>
2023-04-26 18:03:48 +0200

    Navbar überarbeitet (fix) und header.css für funktionalität/ test des headers.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import OnBoarding from "./pages/OnBoarding";
 import LogIn from "./pages/LogIn";
 import TestProfileList from "./components/TestProfileList";
+import Navbar from "./components/header/Navbar";
 function App() {
   return (
         // Darstellung der Test-Funktionen (LogIn-Page und OnBoarding-Page)
     <div className="App">
+        <Navbar/>
      <LogIn/>
         <OnBoarding/>
         <TestProfileList/>
