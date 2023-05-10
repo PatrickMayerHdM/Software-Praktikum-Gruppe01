284d7e61048611461a7a5ddaafdb86d46a614279
PatrickMayerHdM <pmayer2003@gmail.com>
2023-05-10 09:52:02 +0200

    Ausarbeiten des Profile.js damit dieser jetzt auch zumindest Testweise darstellt wie ein Profil auszusehen hat.

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,6 +3,13 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Item from "../theme";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import FormLabel from "@mui/material/FormLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Slider from "@mui/material/Slider";
+import {Button} from "@mui/material";
 
 
 //** Dies soll ein  Profil darstellen. Einerseits das eigene und andererseits ein anderes mögliches Profil, welches
@@ -15,7 +22,81 @@ import Box from "@mui/material/Box";
 function Profile() {
 
     return(
-        <div>Platzhalter</div>
+        <Box sx={{ width: {md: '50%', sm: '60%'} , margin: '0 auto'}}>
+                <Stack direction="column" justifyContent="center" alignItems="center" spacing={1} sx={{ alignItems: 'stretch' }}>
+                    <Item >
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={3}>
+                                Patrick
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={3}>
+                                Mayer
+                            </Grid>
+
+                        </Grid >
+                    </Item>
+                    <Item >
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={2}>
+                                Alter:
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={2}>
+                                20
+                            </Grid>
+                        </Grid >
+                    </Item>
+                    <Item >
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={2}>
+                                Geschlecht:
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={2}>
+                                Männlich
+                            </Grid>
+                        </Grid >
+                    </Item>
+                    <Item>
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={2}>
+                                Körpergröße:
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={2}>
+                                181 cm
+                            </Grid>
+                        </Grid >
+                    </Item>
+                    <Item >
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={2}>
+                                Religion:
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={2}>
+                                Atheist
+                            </Grid>
+                        </Grid >
+                    </Item>
+                    <Item>
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={2}>
+                                Haarfarbe:
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={2}>
+                                Blond
+                            </Grid>
+                        </Grid >
+                    </Item>
+                    <Item>
+                        <Grid container direction="row" justifyContent="center" alignItems="strech" >
+                            <Grid md={4} xs={7} spacing={2}>
+                                Raucher:
+                            </Grid>
+                            <Grid md={4} xs={7} spacing={2}>
+                                Nein
+                            </Grid>
+                        </Grid >
+                    </Item>
+                </Stack>
+            </Box>
     )
 }
 
