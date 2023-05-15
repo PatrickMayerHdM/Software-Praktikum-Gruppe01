e202a157bc4cf77ee7755f1446ae25926d8ae1c0
EfstratiosVassiliou <ev018@hdm-stuttgart.de>
2023-05-15 14:45:36 +0200

    Fehlerbehebung in der App.js (OnBoarding Import wurde nicht gelöscht)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import { Avatar, Menu, MenuItem } from '@mui/material';
 import Item from "./theme";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import OnBoarding from "./pages/OnBoarding";
 import LogIn from "./pages/LogIn";
 import TestProfileList from "./components/TestProfileList";
 import BlockProfileBox from "./components/BlockProfileBox";
