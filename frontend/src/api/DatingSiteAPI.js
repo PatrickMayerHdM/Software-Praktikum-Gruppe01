826341520cf0dca6f9a6af7b6e3f7ea1bbbadf37
Dominik Wunderlich <dw082@hdm-stuttgart.de>
2023-05-16 14:27:44 +0200

    package.json: Proxy hinzugefügt main.py: "header" Zeile gelöscht DatingSiteAPI.js: console.log gelöscht.

--- frontend/src/api/DatingSiteAPI.js
+++ frontend/src/api/DatingSiteAPI.js
@@ -7,7 +7,7 @@ export default class DatingSiteAPI {
     static #api = null;
 
     // Local Python backend
-    #datingServerBaseURL = '/system';
+    #datingServerBaseURL = 'http://localhost:8000/system';
 
     // Local http-fake-backend
     // #datingServerBaseURL = '/hierbennen/system';
@@ -59,7 +59,6 @@ export default class DatingSiteAPI {
             },
             body: JSON.stringify(message)
         }).then((responseJSON) => {
-            console.log("Dreh das Rad!");
             let mssageBO = messageBO.fromJSON(responseJSON)[0];
             return new Promise(function (resolve) {
                 resolve(mssageBO);
