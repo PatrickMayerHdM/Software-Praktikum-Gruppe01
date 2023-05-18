d13406074eb92037b8fccea7d6e5856874d3f59e
Michal Fezer <mf186@hdm-stuttgart.de>
2023-05-18 09:10:02 +0200

    str-Methode in profileNeu umformatiert

--- backend/src/server/bo/profileNEU.py
+++ backend/src/server/bo/profileNEU.py
@@ -1,4 +1,5 @@
 from BusinessObject import BusinessObject as bo
+from Characteristic import Characteristic
 
 
 class profileNeu(bo):
@@ -11,6 +12,7 @@ class profileNeu(bo):
         """self.infoObjectsList = []"""
         self.__blockNote = None
         self.__favoriteNote = None
+        self.__characteristic = Characteristic()
 
     def get_firstname(self):
         return self._firstname
@@ -39,28 +41,23 @@ class profileNeu(bo):
     def set_birthdate(self, value):
         self.__birthdate = value
 
-    """def add_infoObject(self, infoObject):
-        self.infoObjectsList.append(infoObject)"""
-    # Die zwei Methoden waren testweise in der Klasse vorhanden
-    """def get_characteristic(self):
-        return self.__characteristic"""
-
-    def __str__(self):
-        # str Methode gibt das erstellte profil in Form eines String zurück
-        return "profile: {}, {}, {}, {}".format(self.get_id(), self._firstname,
-                                                self._surname, self.__birthdate)
-
 
 """
-profil_eigenschaft = profil.get_characteristic()
-print(profil_eigenschaft.get_age())
+    def set_chars(self, char):
+        self.__characteristic.set_age(char.get_age())
+    
+    def get_chars(self):
+        return self.__characteristic
 
-
-obj = profileNeu()
-obj.set_firstname("Michael")
-obj.set_surname("Fezer")
-obj.set_birthdate("21.10.2001")
-
-
-print(obj)
+    def add_infoObject(self, infoObject):
+        self.infoObjectsList.append(infoObject)
+    Die zwei Methoden waren testweise in der Klasse vorhanden
+    def get_characteristic(self):
+        return self.__characteristic
 """
+
+
+def __str__(self):
+    # str Methode gibt das erstellte profil in Form eines String zurück
+    return "profile: {}, {}, {}, {}".format(self.get_id(), self._firstname,
+                                            self._surname, self.__birthdate)
