04141801fe6497338fc88a86c657b95c56cf22b6
oresttkach <ot014@hdm-stuttgart.de>
2023-05-23 10:51:51 +0200

    ProfileContainsInfoObjects.js erstellt und befüllt.

---
+++ frontend/src/api/ProfileContainsInfoObjects.js
@@ -0,0 +1,79 @@
+import BusinessObject from "./BusinessObject";
+
+/**
+ * Verknüpft die Profile mit den InfoObjects
+ */
+
+export default class profilecontainsobjectsBO extends BusinessObject {
+
+    /**
+     * Baut eine Verknüpfung mit profile_id und infoobj_id
+     *
+     *  @param {*} aprofile_id
+     *  @param {*} ainfoobj_id
+     */
+
+    constructor(aprofile_id, ainfoobj_id) {
+        super();
+        this.profile_id = aprofile_id;
+        this.infoobj_id = ainfoobj_id;
+    }
+
+    /**
+     * Zuweisungs eines Profils in die Verbindungstabelle
+     *
+     * @param {*} aprofile_id - Id
+     */
+
+    setProfileId(aprofile_id){
+        this.profile_id = aprofile_id;
+    }
+
+    /**
+     * Auslesen des Profils
+     */
+
+    getProfileId() {
+        return this.profile_id
+    }
+
+    /**
+     * Zweisung einer Infoobject Id zu einem Profil
+     *
+     * @param {*} ainfoobj_id - Id
+     */
+
+    setInfoobjId(ainfoobj_id) {
+        this.infoobj_id = ainfoobj_id;
+    }
+
+    /**
+     * Liest Infoobjekte aus einem Profil aus
+     */
+
+    getInfoobjId() {
+        return this.infoobj_id
+    }
+
+    /**
+     * Gibt ein Array der ProfileContainsObjectsBO als JSON Struktur zurück
+     */
+
+     static fromJSON(profilecontainsinfoobj) {
+        let result = [];
+
+        if (Array.isArray(profilecontainsinfoobj)) {
+            profilecontainsinfoobj.forEach((p) => {
+                Object.setPrototypeOf(p, profilecontainsinfoobjBO.prototype);
+                result.push(p);
+            })
+        } else {
+            let p = profilecontainsinfoobj
+            Object.setPrototypeOf(p, profilecontainsinfoobjBO.prototype);
+            result.push(p);
+        }
+
+        return result;
+    }
+}
+
