4cee40519174cfbdee34bf0e6a01fac5b849bf70
TaroNakajima <tn053@hdm-stuttgart.de>
2023-05-23 10:54:19 +0200

    Anlegen des Profile-BO's namens Profile.py Anlegen des profileMapper.py

--- backend/src/server/db/profileMapper.py
+++ backend/src/server/db/profileMapper.py
@@ -1,50 +1,24 @@
-from Profile import Profile
 from mapper import mapper
+from Profile import Profile
+from Account import Account
 
-"""Überall wo die Klasse "profile" verwendet wird, müsste eigentlich profileNeu stehen"""
-
-
-class ProfileNeuMapper(mapper):
+class ProfileMapper(mapper):
     def __init__(self):
         super().__init__()
 
-    """List alle "profile" aus und gibt sie als Objekt zurück"""
-
     def find_all(self):
+        """ Auslesen aller Profile """
         result = []
         cursor = self._connection.cursor()
-        cursor.execute("Select * from profiles")
+        cursor.execute("SELECT profile_id, favoriteNote_id, account_id, blockNote_id FROM main.Profile")
         tuples = cursor.fetchall()
 
-        for (profile_id, firstname, surname, birthdate, hair_color, height, smoker, religion) in tuples:
-            profile = profileNeu()
+        for (profile_id, favoriteNote_id, account_id, blockNote_id) in tuples:
+            profile = Profile()
             profile.set_id(profile_id)
-            profile.set_firstname(firstname)
-            profile.set_surname(surname)
-            profile.set_birthdate(birthdate)
-            profile.set_hair_color(hair_color)
-            profile.set_height(height)
-            profile.set_smoker(smoker)
-            profile.set_religion(religion)
-
-        self._connection.commit()
-        cursor.close()
-
-        return result
-
-    def find_by_surname(self, surname):
-        result = []
-        cursor = self._connection.cursor()
-        command = f'SELECT profile_id, firstname, surname, birthdate FROM profiles WHERE firstname LIKE {surname} ORDER BY name'
-        cursor.execute(command)
-        tuples = cursor.fetchall()
-
-        for (profile_id, firstname, surname, birthdate) in tuples:
-            profile = profileNeu()
-            profile.set_id(profile_id)
-            profile.set_firstname(firstname)
-            profile.set_surname(surname)
-            profile.set_birthdate(birthdate)
+            profile.set_favorite_note_id(favoriteNote_id)
+            profile.set_account_id(account_id)
+            profile.set_block_note_id(blockNote_id)
             result.append(profile)
 
         self._connection.commit()
@@ -56,22 +30,20 @@ class ProfileNeuMapper(mapper):
         result = None
 
         cursor = self._connection.cursor()
-        command = f'SELECT profile_id, firstname, surname, birthdate FROM profiles WHERE profile_id={key}'
+        command = f'SELECT profile_id, favoriteNote_id, account_id, blockNote_id FROM main.Profile WHERE profile_id={key}'
         cursor.execute(command)
         tuples = cursor.fetchall()
 
-        try:
-            (profile_id, firstname, surname, birthdate) = tuples[0]
-            profile = profileNeu()
+        if tuples is not None and len(tuples) > 0 and tuples[0] is not None:
+            (profile_id, favorite_note_id, account_id, block_note_id) = tuples[0]
+            profile = Profile()
             profile.set_id(profile_id)
-            profile.set_firstname(firstname)
-            profile.set_surname(surname)
-            profile.set_birthdate(birthdate)
-            result = profile
+            profile.set_favorite_note_id(favorite_note_id)
+            profile.account_id(account_id)
+            profile.blockNote_id(block_note_id)
 
-        except IndexError:
-            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
-            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = profile
+        else:
             result = None
 
         self._connection.commit()
@@ -79,10 +51,35 @@ class ProfileNeuMapper(mapper):
 
         return result
 
+    def find_by_account_id(self, account_id):
+        result = None
+        cursor = self._connection.cursor()
+        command = 'SELECT profile_id, favoriteNote_id, account_id, blockNote_id FROM main.Profile WHERE account_id=%s'
+        data = (account_id,)
+        cursor.execute(command, data)
+        tuples = cursor.fetchall()
+
+        try:
+            (profile_id, favoriteNote_id, account_id, blockNote_id) = tuples[0]
+            p = Profile()
+            p.set_id(profile_id)
+            p.set_favorite_note_id(favoriteNote_id)
+            p.set_account_id(account_id)
+            p.set_block_note_id(blockNote_id)
+            result = p
+
+        except IndexError:
+            "Wenn Tupel leer sind, dann wird IndexError geworfen"
+            result = None
+
+        self._connection.commit()
+        cursor.close()
+        return result
+
     def insert(self, profile):
         # Verbindugn zur DB + cursor-objekt erstellt
         cursor = self._connection.cursor()
-        cursor.execute("SELECT MAX(id) AS maxid FROM profiles")
+        cursor.execute("SELECT MAX(id) AS maxid FROM main.Profile")
         tuples = cursor.fetchall()
 
         for (maxid) in tuples:
@@ -94,42 +91,34 @@ class ProfileNeuMapper(mapper):
                 """Wenn keine id vorhanden ist, beginnen wir mit der id 1"""
                 profile.set_id(1)
 
-        command = "INSERT INTO profiles (profile_id, firstname, surname, birthdate) VALUES (%s, %s, %s, %s)"
-        data = (profile.get_id(), profile.get_firstname(), profile.get_surname(), profile.get_birthdate())
+        command = "INSERT INTO main.Profile (profile_id, favorite_note_id, account_id, block_note_id) VALUES (%s, %s, %s, %s)"
+        data = (profile.get_id(), profile.get_favorite_note_id(), profile.get_account_id(), profile.get_block_note_id())
         cursor.execute(command, data)
 
         self._connection.commit()
         cursor.close()
 
-        return profileNeu
+        return profile
 
     def update(self, profile):
-        # Verbindugn zur DB + cursor-objekt erstellt
+        """ Aktualisierung einer Profil-Instanz"""
         cursor = self._connection.cursor()
 
-        # SQL-Befehl um Datensatz in DB zu aktualisieren (Datensatz mit ID, welche in profileNeu gespeichert ist)
-        command = "UPDATE profiles" + "SET firstname=%s, surname=%s, birthdate=%s WHERE id=%s "
+        command = "UPDATE main.Profile SET profile_id=%s, favoriteNote_id=%s, account_id=%s, block_note_id=%s"
+        data = (profile.get_id(), profile.get_favorite_note_id(), profile.get_account_id(), profile.get_block_note_id())
 
-        # Speichern der gegebenen parameter als Tupel und Ausführung mit aktualisiertet Daten durch execute
-        data = (profile.get_firstname(), profile.get_surname(), profile.get_birthdate(), profile.get_id())
         cursor.execute(command, data)
 
-        # Speicherung der veränderten DB und Schließung des cursor
         self._connection.commit()
         cursor.close()
 
     def delete(self, profile):
+        """ Löschen eines Datensatzes """
         cursor = self._connection.cursor()
 
-        command = "DELETE FROM profiles WHERE id={}".format(profile.get_id())
+        command = f"DELETE FROM main.Profile WHERE profile_id = {profile.get_id()}"
         cursor.execute(command)
 
         self._connection.commit()
         cursor.close()
 
-
-if (__name__ == "__main__"):
-    with ProfileNeuMapper() as mapper:
-        result = mapper.find_all()
-        for profile in result:
-            print(profile)
