af809af72762adedb42fdf2a0b2fedeef6034492
TaroNakajima <tn053@hdm-stuttgart.de>
2023-06-09 11:18:58 +0200

    - Anpassen Icon Search.js für "neue Profile anzeigen"

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import React from "react";
-import BubbleChartIcon from '@mui/icons-material/BubbleChart';
 import EditIcon from '@mui/icons-material/Edit';
 import "../components/Profile.css";
 import SearchIcon from '@mui/icons-material/Search';
@@ -12,6 +11,7 @@ import { Link } from "react-router-dom"
 import DatingSiteAPI from "../api/DatingSiteAPI";
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import UpdateIcon from '@mui/icons-material/Update';
 
 /**
  * Innerhalb der Suche gibt es für den User später verschiedene Optionen.
@@ -203,7 +203,7 @@ class Search extends React.Component{
                                       cursor: "pointer"
                                     }}
                                   >
-                                    <BubbleChartIcon/>
+                                    <UpdateIcon/>
                                   </button>
                                 </Grid>
 
