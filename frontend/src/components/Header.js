bf02aecf2f3f391377a6c07a338006fe00a76053
TaroNakajima <tn053@hdm-stuttgart.de>
2023-06-09 11:05:01 +0200

    - Anpassen der Header.js (Darstellung Nav-Bar)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
         <div className="header">
             <Link to={`/Profil/${this.props.user.uid}`}>
                 <IconButton>
-                    <PersonIcon className="hader_icon" />
+                    <PersonIcon className="header_icon" />
                 </IconButton>
             </Link>
             <Link to="/Suche">
