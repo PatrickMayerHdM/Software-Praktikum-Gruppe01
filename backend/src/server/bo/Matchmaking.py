f4914b343e36ef1f9c90dfc58bb038f5197196bb
oresttkach <ot014@hdm-stuttgart.de>
2023-06-13 11:33:01 +0200

    Matchmaking Algorithmus erweitert und Datenbank angebunden.

--- backend/src/server/bo/Matchmaking.py
+++ backend/src/server/bo/Matchmaking.py
@@ -4,38 +4,23 @@
 Prozentwert zurück, der angibt, wie gut diese zusammenpassen. Dies wird für sämtliche Profile durchgefüht.
 Der Prozentuale wert je Profil wird zurückgegeben und den Nutzer angezeigt."""
 
+    # Sprachauswertung. Bei dieser wird zuerst geprft, ob die Eigenschaften
+    # der durch den user definierten Eingaben ähnlich genug sind.
+    # Anschließend wird die Ähnlichkeit der Info-Objekte verglichen.
 class Matchmaking:
     def __init__(self, searchprofile, userprofile):
         self.searchprofile = searchprofile
-        self.userprofile = userprofile
+        self.userprofile =userprofile
 
-#Es müssen die InfoObjekte und die Markmale von Suchprofil und Uerprofil geholt werden.
-    def get_infoobjects_searchprofile(self):
-        return #infoobjects_searchprofile
-
-    def get_infoobjects_userprofile(self):
-        return #infoobjects_userprofiles
-
-    def get_measures_searchprofile(self):
-        return #measures_searchprfile
-
-    def get_measures_userprfile(self):
-        return #measures_userprofile
-
-    def calc_percentage(self):
-        total_infoobjects = len(self)
+    def percentage(self):
+        total_infoobjects = len(self.searchprofile.get_infoobjects)
         total_count = 0
 
-    #In der For-Schleife wird zuerst geprüft, ob das infoobjekt in beiden profilen vorhanden ist.
-    #Anschließend wird gprüft, ob die infoobjekte übereinstimmen
         for infoobject in self.searchprofile:
             if infoobject in self.userprofile and self.searchprofile[infoobject] == self.userprofile[infoobject]:
                 total_count += 1
 
-        total_percentage = (total_count / total_infoobjects) * 100
-        return total_percentage
+        # Sprachauswertung kommt hierhin und muss in total percentage hinzugefgt werden.
 
-    def matchmaking(self):
-        match_score = self.calc_percentage()
-        return match_score
-
+        total_percentage = (total_count / total_infoobjects) *100
+        return  total_percentage
