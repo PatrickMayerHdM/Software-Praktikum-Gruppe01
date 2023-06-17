3e900001392448a81b428c4dd851707091f6ccf1
oresttkach <ot014@hdm-stuttgart.de>
2023-06-17 10:18:57 +0200

    Neuer MatchmakingMapper.py. Jetzt mit guter Funktionalität. Gibt dsa richtige Dict aus.

--- backend/src/server/db/MatchmakingMapper.py
+++ backend/src/server/db/MatchmakingMapper.py
@@ -1,58 +1,79 @@
+from server.bo.InfoObject import InfoObject
 from server.db.mapper import mapper
-from server.bo.Profile import Profile
+
 
 class MatchmakingMapper(mapper):
+    """ Mapper-Klasse, die das Ähnlichkeitsmaß auf eine relationale Datenbank abbildet."""
+
     def __init__(self):
         super().__init__()
 
-
-def find_info_object_by_profile_id(self, key):
-    result = []
-
-    """ Auslesen der Profil Id nach Key """
-
-    cursor = self._connection.cursor()
-    command = f"SELECT char_value FROM main.InfoObject WHERE (profile_id='{key}')"
-    cursor.execute(command)
-    tuples = cursor.fetchall()
-    print(tuples)
-
-    for (infoobject_id, char_value) in tuples:
-        info_obj = InfoObject()
-        info_obj.set_id(infoobject_id)
-        info_obj.set_value(char_value)
-        result.append(info_obj)
-
-    self._connection.commit()
-    cursor.close()
-
-    print("result: ", result)
-    return result
-
-    def find_profile_id_by_key(self, key):
-        result = None
-
+    def find_info_by_profile(self, profile_id):
+        """ Auslesen der Info-Objekte zweier Profile. """
+        result = []
         cursor = self._connection.cursor()
-        command = f'SELECT profile_id FROM main.profile WHERE google_fk=%s'
-        data = (key,)
-        cursor.execute(command, data)
+        command = f"SELECT infoobject_id, char_id, char_value, profile_id FROM main.infoobject WHERE profile_id='{profile_id}'"
+        cursor.execute(command)
         tuples = cursor.fetchall()
 
-        if tuples is not None and len(tuples) > 0 and tuples[0] is not None:
-            (profile_id) = tuples[0]
-            profile = Profile()
-            profile.set_id(profile_id)
-            print("Profil von der Datenbank im Mapper:", profile)
-
-            result = profile
-        else:
-            result = None
+        for (infoobject_id, char_id, char_value, profile_id) in tuples:
+            inf = InfoObject()
+            inf.set_id(infoobject_id)
+            inf.set_char_fk(char_id)
+            inf.set_value(char_value)
+            inf.set_profile_fk(profile_id)
+            result.append(inf)
 
         self._connection.commit()
         cursor.close()
 
         return result
 
-    def fhfhfhfhndhcncdhddcndhcn
 
+    def compare_info_objects(self, profile_id1, profile_id2):
+        info_objects1 = self.find_info_by_profile(profile_id1)
+        info_objects2 = self.find_info_by_profile(profile_id2)
 
+        matches = []
+        for info1 in info_objects1:
+            for info2 in info_objects2:
+                if info1.get_char_fk() == info2.get_char_fk():
+                    match = {
+                        "Profile ID 1": profile_id1,
+                        "Profile ID 2": profile_id2,
+                        "InfoObject ID P1": info1.get_id(),
+                        "InfoObject ID P2": info2.get_id(),
+                        "Char ID": info1.get_char_fk(),
+                        "Char Value 1": info1.get_value(),
+                        "Char Value 2": info2.get_value()
+                    }
+                    matches.append(match)
+
+        return matches
+
+    def find_matching_info_objects(self, profile_id1, profile_id2):
+        info_objects1 = self.find_info_by_profile(profile_id1)
+        info_objects2 = self.find_info_by_profile(profile_id2)
+
+        matching_info_objects = []
+        for info1 in info_objects1:
+            for info2 in info_objects2:
+                if info1.get_char_fk() == info2.get_char_fk() and info1.get_value() == info2.get_value():
+                    matching_info_objects.append(info1)
+
+        return matching_info_objects
+
+    def insert(self, object):
+        pass
+
+    def delete(self, object):
+        pass
+
+    def update(self, object):
+        pass
+
+    def find_all(self):
+        pass
+
+    def find_by_key(self, key):
+        pass
