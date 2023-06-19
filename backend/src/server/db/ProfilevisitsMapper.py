667c55da563deab5314ffc1b7ff1e68c45fb9fc6
TaroNakajima <tn053@hdm-stuttgart.de>
2023-06-19 17:39:19 +0200

    Bug beheben, sodass bei der Profilevisits-Tabelle eine Person nicht mehrmals hinzugefügt werden kann.

--- backend/src/server/db/ProfilevisitsMapper.py
+++ backend/src/server/db/ProfilevisitsMapper.py
@@ -24,23 +24,53 @@ class ProfilevisitsMapper(mapper):
 
         return results
 
-    def insert(self, visitedprofile):
-        print("insert in ProfilevisitsMapper",visitedprofile.get_visitedprofile_id)
-        cursor = self._connection.cursor()
-        cursor.execute("SELECT MAX(profilevisits_id) AS maxid FROM main.Profilevisits")
-        tuples = cursor.fetchall()
+    # def insert(self, visitedprofile):
+    #     print("insert in ProfilevisitsMapper",visitedprofile.get_visitedprofile_id)
+    #     cursor = self._connection.cursor()
+    #     cursor.execute("SELECT MAX(profilevisits_id) AS maxid FROM main.Profilevisits")
+    #     tuples = cursor.fetchall()
+    #
+    #     for (maxid) in tuples:
+    #         if maxid[0] is not None:
+    #             """Wenn eine ID vorhanden ist, zählen wir diese um 1 hoch"""
+    #             visitedprofile.set_id(maxid[0] + 1)
+    #         else:
+    #             """Wenn keine id vorhanden ist, beginnen wir mit der id 1"""
+    #             visitedprofile.set_id(1)
+    #
+    #     command = "INSERT INTO main.Profilevisits (profilevisits_id, mainprofile_id, visitedprofile_id) VALUES (%s, %s, %s)"
+    #     data = (visitedprofile.get_id(), visitedprofile.get_mainprofile_id(), visitedprofile.get_visitedprofile_id())
+    #     cursor.execute(command, data)
+    #
+    #     self._connection.commit()
+    #     cursor.close()
+    #
+    #     return visitedprofile
 
-        for (maxid) in tuples:
-            if maxid[0] is not None:
-                """Wenn eine ID vorhanden ist, zählen wir diese um 1 hoch"""
-                visitedprofile.set_id(maxid[0] + 1)
+    def insert(self, visitedprofile):
+        print("insert in ProfilevisitsMapper", visitedprofile.get_visitedprofile_id)
+        cursor = self._connection.cursor()
+        command = "SELECT profilevisits_id FROM main.Profilevisits WHERE mainprofile_id = %s AND visitedprofile_id = %s"
+        data = (visitedprofile.get_mainprofile_id(), visitedprofile.get_visitedprofile_id())
+        cursor.execute(command, data)
+        """ Abfrage, ob bereits ein Eintrag für genau diese mainprofile_id und visitedprofile_id existiert. """
+        existing_id = cursor.fetchone()
+
+        if existing_id is not None:
+            """ Falls ein Profil zuvor noch nicht angesehen wurde, wird es Profilevisits mit einem neuen Eintrag hinzugefügt. """
+            visitedprofile.set_id(existing_id[0])
+        else:
+            cursor.execute("SELECT MAX(profilevisits_id) AS maxid FROM main.Profilevisits")
+            maxid = cursor.fetchone()[0]
+            if maxid is not None:
+                visitedprofile.set_id(maxid + 1)
             else:
-                """Wenn keine id vorhanden ist, beginnen wir mit der id 1"""
                 visitedprofile.set_id(1)
 
-        command = "INSERT INTO main.Profilevisits (profilevisits_id, mainprofile_id, visitedprofile_id) VALUES (%s, %s, %s)"
-        data = (visitedprofile.get_id(), visitedprofile.get_mainprofile_id(), visitedprofile.get_visitedprofile_id())
-        cursor.execute(command, data)
+            insert_command = "INSERT INTO main.Profilevisits (profilevisits_id, mainprofile_id, visitedprofile_id) VALUES (%s, %s, %s)"
+            insert_data = (
+            visitedprofile.get_id(), visitedprofile.get_mainprofile_id(), visitedprofile.get_visitedprofile_id())
+            cursor.execute(insert_command, insert_data)
 
         self._connection.commit()
         cursor.close()
